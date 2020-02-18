67ccd27d2fd95882609f5a96c22c23dc01bc8628
Yuriy Zdyrko <yuriy.zdyrko@gmail.com>
2020-02-18 11:22:41 +0200

    ch 3 - state machine

---
+++ islands_engine/lib/islands_engine/ch_3_test.ex
@@ -0,0 +1,14 @@
+defmodule Chapter3Test do
+    alias IslandsEngine.Rules
+
+    def run do
+        r_initialized = Rules.new()
+        {:ok, r_players_added} = Rules.check(r_initialized, :add_player)
+        {:ok, r_positioned_islands} = Rules.check(r_players_added, {:position_islands, :player1})
+        {:ok, r_set_islands_player_1} = Rules.check(r_positioned_islands, {:set_islands, :player1})
+        {:ok, r_player1_turn} = Rules.check(r_set_islands_player_1, {:set_islands, :player2})
+        {:ok, r_player2_turn} = Rules.check(r_player1_turn, {:guess_coordinate, :player1})
+        {:ok, r_player1_turn} = Rules.check(r_player2_turn, {:guess_coordinate, :player2})
+        {:ok, r_game_over} = Rules.check(r_player2_turn, {:win_check, :win})
+    end
+end
