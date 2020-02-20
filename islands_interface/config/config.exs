66573342465d6707e2116be9339a9c21061c9139
banzay <yuriy.zdyrko@gmail.com>
2020-02-20 17:02:03 +0200

    ch 6 - Phoenix

---
+++ islands_interface/config/config.exs
@@ -0,0 +1,28 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+# Configures the endpoint
+config :islands_interface, IslandsInterfaceWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "CZVjO1YceUbv7AtDe6SpTmpE96fxqYb/Ff2DMtHhWwKgC/Ncyh1TSWZCgkEB1tW3",
+  render_errors: [view: IslandsInterfaceWeb.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: IslandsInterface.PubSub, adapter: Phoenix.PubSub.PG2],
+  live_view: [signing_salt: "4kTyz1So"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
